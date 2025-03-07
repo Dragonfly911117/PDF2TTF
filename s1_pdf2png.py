9a3d12179f33fa6d194bf13081b6e7aebbeb49e1
df <dragonflybaby2.0@gmail.com>
2025-03-07 10:33:53 +0800

    Enhance: Make s1_pdf2png can read config file

--- s1_pdf2png.py
+++ s1_pdf2png.py
@@ -2,24 +2,21 @@ from pdf2image import convert_from_path
 from tqdm import tqdm
 import os
 
-# 設定 Poppler 路徑 (請確認你的 Poppler 安裝位置)
-poppler_path = r"C:\Users\User\.conda\envs\fontenv\Library\bin" 
+import toml
+
+config = toml.load("./config.toml")
+
 
 # 轉換 PDF 為 PNG 圖片
 def pdf_to_png(pdf_path, output_folder, start_page=1, end_page=None, dpi=600):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    print("讀取 PDF 文件中，請稍等…")
-
     try:
+        print("讀取 PDF 文件中，請稍等…")
         # 轉換 PDF 頁面為圖片
         images = convert_from_path(
             pdf_path,
             dpi=dpi,
             first_page=start_page,
             last_page=end_page,
-            poppler_path=poppler_path
         )
 
         # 進度條顯示
@@ -28,13 +25,18 @@ def pdf_to_png(pdf_path, output_folder, start_page=1, end_page=None, dpi=600):
 
         print(f"轉換完成！圖片存儲於：{output_folder}")
 
-    except Exception as e:
+    except Exceptiona as e:
         print(f"發生錯誤：{e}")
 
 # 使用者輸入
-pdf_path = input("請輸入 PDF 文件的完整路徑（包含 .pdf）：").strip()
-output_folder = input("請輸入輸出圖片的資料夾（設定為rotated_你的學號）：").strip()
-start_page = int(input("請輸入要切割的 PDF 起始頁：").strip())
-end_page = int(input("請輸入要切割的 PDF 結束頁：").strip())
+pdf_path = config['src']['file_name'].strip()
+output_folder = config['path']['page_png'].strip()
+start_page = config['src']['starting_page']
+end_page = config['src']['ending_page']
+
+assert os.path.exists(pdf_path), "src.file_name is not found!"
+assert start_page <= end_page, "src.starting_page is not <= src.ending_page"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 pdf_to_png(pdf_path, output_folder, start_page, end_page)
